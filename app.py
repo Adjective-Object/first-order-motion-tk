5b7b4be68545efc3605e362e81c09f9dc9dcd966
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2021-06-02 13:32:17 -0400

    basic app wrapper

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os.path
-from typing import DefaultDict
 import cv2
 import torch
 import zipfile
-import imageio
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import gdown
 import warnings
 from PIL import ImageTk, Image
@@ -20,13 +17,14 @@ from demo import load_checkpoints
 import tkinter as tk
 
 class VideoCapture(tk.Widget):
-    def __init__(self, parent):
+    def __init__(self, parent, oncamloaded):
         tk.Frame.__init__(self, parent)
 
         self.refresh_button = tk.Button(self)
         self.refresh_button["text"] = "Refresh Camera List"
         self.refresh_button["command"] = self.update_camera_list_and_repack
 
+        self.oncamloaded = oncamloaded
 
         self.reopen_button = tk.Button(self)
         self.reopen_button["text"] = "Reopen Current Camera"
@@ -43,7 +41,7 @@ class VideoCapture(tk.Widget):
         self.repack()
         self.selected_camera = tk.StringVar()
         self.selected_camera.trace('w', self.update_capture)
-        self.update_camera_list_and_repack()
+        self.after(1, self.update_camera_list_and_repack)
     
     def repack(self):
         self.refresh_button.pack(side="bottom")
@@ -66,7 +64,9 @@ class VideoCapture(tk.Widget):
                 self.imgtk = None
                 self.img = None
                 self.repack()
+                self.oncamloaded(self.cap)
             else:
+                self.oncamloaded(None)
                 self.error_label["text"] = ""
                 self.repack()
 
@@ -120,40 +120,45 @@ class VideoCapture(tk.Widget):
     def get(self):
         return self.entry.get()
 
-class Application(tk.Frame):
+class GetInputsApplication(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.filename = "source_image_inputs/the_rock_colorkey.jpeg";
+        self.filename = tk.StringVar(self, "source_image_inputs/the_rock_colorkey.jpeg")
 
         self.master = master
         self.pack()
         self.create_widgets()
 
+        self.check_steal_button()
+
     def create_widgets(self):
+        self.steal_button = tk.Button(self)
+        self.steal_button["command"] = self.run_stolen_face
+        self.pack_steal_button()
         self.hi_there = tk.Button(self)
         self.pack_button()
         self.image_label = tk.Label(self)
         self.pack_preview_img()
-        self.video_capture = VideoCapture(self)
+        self.video_capture = VideoCapture(self, self.check_steal_button)
         self.pack_videocapture()
 
     def pack_steal_button(self):
+        self.steal_button["text"] = "Borrow that face!"
+        self.steal_button["state"] = tk.DISABLED
         self.steal_button.pack(side="bottom")
-
 
     def pack_videocapture(self):
         self.video_capture.pack(side="bottom")
 
     def pack_preview_img(self):
-        print(self.filename)
-        self.img = Image.open(self.filename).resize((255,255))
+        self.img = Image.open(self.filename.get()).resize((256,256))
         self.photo_img = ImageTk.PhotoImage(self.img)
         self.image_label.configure(image=self.photo_img)
         self.image_label.image=self.photo_img
         self.image_label.pack(side="left")
     
     def pack_button(self):
-        self.hi_there["text"] = os.path.basename(self.filename)
+        self.hi_there["text"] = os.path.basename(self.filename.get())
         self.hi_there["command"] = self.update_img
         self.hi_there.pack(side="left")
 
@@ -164,112 +169,140 @@ class Application(tk.Frame):
             filetypes=(("Image Files", "*.png *.jpg *.jpeg *.bmp"), ("all files", "*.*")),
         )
         if filename is not None:
-            self.filename = filename
+            self.filename.set(filename)
             self.pack_preview_img()
 
             self.pack_button()
             self.pack_preview_img()
+            self.check_steal_button()
+
+    def check_steal_button(self, *argv):
+        if self.img is not None and self.video_capture.cap is not None and self.video_capture.cap.isOpened():
+            self.steal_button["state"] = tk.ACTIVE
+        else:
+            self.steal_button["state"] = tk.DISABLED
+
+    def run_stolen_face(self):
+        global param_inputimg
+        global param_inputcap
+        if self.img is not None and self.video_capture.cap is not None and self.video_capture.cap.isOpened():
+            param_inputimg = self.img
+            param_inputcap = self.video_capture.cap
+            self.quit()
+
 
 param_inputimg = None
 pram_inputcap = None
 
 root = tk.Tk()
-app = Application(master=root)
+app = GetInputsApplication(master=root)
 app.mainloop()
+app.destroy()
+root.destroy()
+
+def prep_frame(frame):
+    frame = cv2.flip(frame, 1)
+
+    w = int( min(*frame.shape[0:2]) * 0.8)
+    h = w
+    x = frame.shape[1]//2 - w // 2 
+    y = frame.shape[0]//2 - h // 2
+
+    frame = frame[y : y + h, x : x + w,:]
+
+    return resize(frame, (256, 256))[..., :3]
 
 
+if param_inputimg and param_inputcap:
+    USE_CPU = False
 
+    relative = True
+    adapt_movement_scale = False
 
-# USE_CPU = False
+    source_image = np.asarray(param_inputimg)
+    print(source_image.shape)
+    source_image = source_image[..., :3] / 255
+    print(source_image.shape)
+    cap = param_inputcap
 
-# if not os.path.exists("temp"):
-#     os.mkdir("temp")
+    # render just the camera for a frame while we load
+    ret, frame = cap.read()
+    frame = prep_frame(frame)
+    cv2.imshow("Face Borrower", frame)
 
-# relative = True
-# adapt_movement_scale = False
+    if not os.path.exists("temp"):
+        os.mkdir("temp")
 
+    model_checkpoint_exist = os.path.exists("extract/vox-cpk.pth.tar")
+    if not model_checkpoint_exist:
+        url = "https://drive.google.com/uc?id=1wCzJP1XJNB04vEORZvPjNz6drkXm5AUK"
+        output = "temp/checkpoints.zip"
+        gdown.download(url, output, quiet=False)
+        with zipfile.ZipFile(output, "r") as zip_ref:
+            zip_ref.extractall("extract")
 
-# model_checkpoint_exist = os.path.exists("extract/vox-cpk.pth.tar")
-# if not model_checkpoint_exist:
-#     url = "https://drive.google.com/uc?id=1wCzJP1XJNB04vEORZvPjNz6drkXm5AUK"
-#     output = "temp/checkpoints.zip"
-#     gdown.download(url, output, quiet=False)
-#     with zipfile.ZipFile(output, "r") as zip_ref:
-#         zip_ref.extractall("extract")
+    generator, kp_detector = load_checkpoints(
+        config_path="config/vox-256.yaml",
+        checkpoint_path="extract/vox-cpk.pth.tar",
+        cpu=USE_CPU,
+    )
 
-# generator, kp_detector = load_checkpoints(
-#     config_path="config/vox-256.yaml",
-#     checkpoint_path="extract/vox-cpk.pth.tar",
-#     cpu=USE_CPU,
-# )
+    count = 0
+    try:
+        while True:
 
-# source_image = resize(
-#     imageio.imread("source_image_inputs/harry-potter-3-colorkey.png"), (256, 256)
-# )[..., :3]
+            ret, frame = cap.read()
 
-# fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-# # out1 = cv2.VideoWriter('temp/animation_from_webcam.avi', fourcc, 12, (256*3 , 256), True)
-# cv2_source = cv2.cvtColor(source_image.astype("float32"), cv2.COLOR_BGR2RGB)
-# cap = cv2.VideoCapture(1)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            with torch.no_grad():
+                predictions = []
+                source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(
+                    0, 3, 1, 2
+                )
+                source = source.cuda()
+                kp_source = kp_detector(source)
+                ims = [source_image]
+                frame = prep_frame(frame)
 
-# count = 0
-# try:
-#     while True:
+                if count == 0:
+                    source_image1 = frame
+                    source1 = torch.tensor(
+                        source_image1[np.newaxis].astype(np.float32)
+                    ).permute(0, 3, 1, 2)
+                    kp_driving_initial = kp_detector(source1)
 
-#         ret, frame = cap.read()
-#         if cv2.waitKey(1) & 0xFF == ord("q"):
-#             break
-#         with torch.no_grad():
-#             predictions = []
-#             source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(
-#                 0, 3, 1, 2
-#             )
-#             if not USE_CPU:
-#                 source = source.cuda()
-#             kp_source = kp_detector(source)
-#             ims = [source_image]
-#             frame = cv2.flip(frame, 1)
-#             frame = frame[y : y + h, x : x + w]
-#             frame1 = resize(frame, (256, 256))[..., :3]
+                if count % 1 == 0:
+                    frame_test = torch.tensor(
+                        frame[np.newaxis].astype(np.float32)
+                    ).permute(0, 3, 1, 2)
 
-#             if count == 0:
-#                 source_image1 = frame1
-#                 source1 = torch.tensor(
-#                     source_image1[np.newaxis].astype(np.float32)
-#                 ).permute(0, 3, 1, 2)
-#                 kp_driving_initial = kp_detector(source1)
+                    driving_frame = frame_test
+                    if not USE_CPU:
+                        driving_frame = driving_frame.cuda()
+                    kp_driving = kp_detector(driving_frame)
+                    kp_norm = normalize_kp(
+                        kp_source=kp_source,
+                        kp_driving=kp_driving,
+                        kp_driving_initial=kp_driving_initial,
+                        use_relative_movement=relative,
+                        use_relative_jacobian=relative,
+                        adapt_movement_scale=adapt_movement_scale,
+                    )
+                    out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
+                    #         predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
+                    im = np.transpose(out["prediction"].data.cpu().numpy(), [0, 2, 3, 1])[0]
+                    im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+                joinedFrame = np.concatenate((im, frame), axis=1)
 
-#             if count % 2 == 0:
-#                 frame_test = torch.tensor(
-#                     frame1[np.newaxis].astype(np.float32)
-#                 ).permute(0, 3, 1, 2)
+                #         joinedFrameToSave = np.uint8(256 * joinedFrame)
+                #             out1.write(joinedFrameToSave)
 
-#                 driving_frame = frame_test
-#                 if not USE_CPU:
-#                     driving_frame = driving_frame.cuda()
-#                 kp_driving = kp_detector(driving_frame)
-#                 kp_norm = normalize_kp(
-#                     kp_source=kp_source,
-#                     kp_driving=kp_driving,
-#                     kp_driving_initial=kp_driving_initial,
-#                     use_relative_movement=relative,
-#                     use_relative_jacobian=relative,
-#                     adapt_movement_scale=adapt_movement_scale,
-#                 )
-#                 out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
-#                 #         predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
-#                 im = np.transpose(out["prediction"].data.cpu().numpy(), [0, 2, 3, 1])[0]
-#                 im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-#             joinedFrame = np.concatenate((im, frame1), axis=1)
+                cv2.imshow("Face Borrower", joinedFrame)
 
-#             #         joinedFrameToSave = np.uint8(255 * joinedFrame)
-#             #             out1.write(joinedFrameToSave)
-
-#             cv2.imshow("Test", joinedFrame)
-
-#             count += 1
-# except:
-#     raise
-# finally:
-#     cap.release()
-#     cv2.destroyAllWindows()
+                count += 1
+    except:
+        raise
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 xhost +
 
-podman run --rm -it --device=/dev/video1:/dev/video1 \
+podman run --rm -it --device=/dev/video2:/dev/video2 \
        	--env DISPLAY=$DISPLAY \
         --env="QT_X11_NO_MITSHM=1" \
         --env="__NV_PRIME_RENDER_OFFLOAD=1" \
         --env="__GLX_VENDOR_LIBRARY_NAME=nvidia" \
         --env="__VK_LAYER_NV_optimus=NVIDIA_only" \
-        -v /dev/video1:/dev/video1 \
+        -v /dev/video2:/dev/video2 \
         -v /tmp/.X11-unix:/tmp/.X11-unix:ro  \
         -v "${PWD}:/app" \
        	-p 8888:8888 -p 6006:6006 \
